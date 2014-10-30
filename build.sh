184f4e421f0159d74b21288f49e74706473fa2d4
Kurt Harriger and Bob Cotton <kharriger@rallydev.com>
2014-10-30 14:24:05 -0600

    Add build script to modify version attribute since env properties do not appear to work here

---
+++ build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -e
+
+# Must add the following to repo to the default profile in .m2/settings.xml
+# <repositories>
+#    <repository>
+#         		<id>Cloudera</id>
+#         		<name>Cloudera repository</name>
+#         		<url>https://repository.cloudera.com/content/repositories/releases</url>
+#         		<releases>
+#         			<enabled>true</enabled>
+#         		</releases>
+#         		<snapshots>
+#         			<enabled>false</enabled>
+#         		</snapshots>
+#         	</repository>
+#  </repositories> 
+
+function set-version() {
+	VERSION=$1
+	xmlstarlet edit -L -N x="http://maven.apache.org/POM/4.0.0" -u "//x:project/x:version" -v "$VERSION"  pom.xml
+	xmlstarlet edit -L -N x="http://maven.apache.org/POM/4.0.0" -u "//x:project/x:properties/x:project.version" -v "$VERSION"  pom.xml
+	find . -name "pom.xml" | xargs -P1 xmlstarlet edit -L -N x="http://maven.apache.org/POM/4.0.0" -u "//x:project/x:parent/x:version" -v "$VERSION" 
+}
+
+which xmlstarlet > /dev/null || {
+  echo "Please install xmlstarlet" 
+  exit 2
+}
+
+if git status | grep pom.xml > /dev/null; then 
+  echo "This script modifies pom.xml files.  Please commit, stash, or revert any changes"
+  exit 3;
+fi
+
+# Clean
+rm -rf mvn-repo
+
+# Deploy required dependency not in maven central public repo
+mvn deploy:deploy-file -DgroupId=org.apache.avro -DartifactId=avro-repo-bundle -Dversion=1.7.4-SNAPSHOT -Dpackaging=jar -Dfile=lib/avro-repo-bundle-1.7.4-SNAPSHOT-withdeps.jar   -Durl=file://$(pwd)/target/mvn-repo
+
+# --- Build Hadoop 1 ---
+# update pom files
+set-version 0.1.0-hadoop1
+mvn -DaltDeploymentRepository=internal.repo::default::file://$(pwd)/mvn-repo clean deploy
+find . -name "pom.xml" | xargs git checkout 
+
+# --- Build Cloudera ---
+# update pom files
+set-version 0.1.0-cdh4
+mvn -Dhadoop-client.version=2.0.0-mr1-cdh4.2.0 -DaltDeploymentRepository=internal.repo::default::file://$(pwd)/target/mvn-repo clean deploy
+find . -name "pom.xml" | xargs git checkout 
+
+# Upload mvn-repo to s3
+aws --profile schwartz-ci s3 sync target/mvn-repo s3://rally-maven-public/ 
